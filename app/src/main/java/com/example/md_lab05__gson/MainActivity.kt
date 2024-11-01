026170e001149dd34ff86cb4ff2092ee04b25943
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-11-01 16:17:51 +0700

    add gson implementation

--- app/src/main/java/com/example/md_lab05__gson/MainActivity.kt
+++ app/src/main/java/com/example/md_lab05__gson/MainActivity.kt
@@ -21,4 +21,28 @@ class MainActivity : AppCompatActivity() {
 
         plant(Timber.DebugTree())
     }
+
+    data class Photo(
+        val id: String,
+        val owner: String,
+        val secret: String,
+        val server: String,
+        val farm: Int,
+        val title: String,
+        val isPublic: Int,
+        val isFriend: Int,
+        val isFamily: Int
+    )
+
+    data class PhotoPage(
+        val page: Int,
+        val pages: Int,
+        val perPage: Int,
+        val total: Int,
+        val photo: List<Photo>
+    )
+
+    data class Wrapper(
+        val photos: PhotoPage
+    )
 }
