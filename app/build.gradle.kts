0ff6bc2793b60cdbd91f5bd93c2249dbb55897b8
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-11-26 07:31:39 +0700

    complete task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.md_lab05__gson"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.md_lab05__gson"
@@ -42,6 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.timber)
+    implementation(libs.gson)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/md_lab05__gson/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.md_lab05__gson
+
+data class Contact(
+    val name: String,
+    val phone: String,
+    val type: String
+)
